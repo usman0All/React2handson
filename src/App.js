dc788e46fbcb93885daf045294e98d97f2dec42f
mohd usman <usman12797@gmail.com>
2023-09-05 18:52:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Classcompo from './Classcompo';
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1 id="heading">EMPLOYEE FEEDBACK FORM</h1>
+    <Classcompo/>
+    </div>
+  );
+}
+
+export default App;
